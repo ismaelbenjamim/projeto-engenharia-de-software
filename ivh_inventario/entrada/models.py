f15786a1bb638f893ba3d533adda5dacd4add705
ismaelbenjamim <ismaelbenjamim15@hotmail.com>
2023-01-26 10:16:39 -0300

    Adicionado model de entrada

--- ivh_inventario/entrada/models.py
+++ ivh_inventario/entrada/models.py
@@ -1,3 +1,14 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+from ivh_inventario.item.models import Item
+from ivh_inventario.usuario.models import Usuario
+
+
+class Entrada(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid4, primary_key=True, editable=False)
+    dt_entrada = models.DateField("Data de entrada")
+    quantidade = models.IntegerField("Quantidade", default=1)
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_NULL, null=True, blank=True)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
