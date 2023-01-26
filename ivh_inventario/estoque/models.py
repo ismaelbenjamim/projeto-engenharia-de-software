b58f36ca514a470c59942c8080f93584b813b0e2
ismaelbenjamim <ismaelbenjamim15@hotmail.com>
2023-01-26 10:17:12 -0300

    Adicionado model de estoque

--- ivh_inventario/estoque/models.py
+++ ivh_inventario/estoque/models.py
@@ -1,3 +1,11 @@
+from uuid import uuid4
+
 from django.db import models
 
-# Create your models here.
+from ivh_inventario.item.models import Item
+
+
+class Estoque(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid4, primary_key=True, editable=False)
+    estoque_atual = models.IntegerField("Estoque atual", default=1)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
