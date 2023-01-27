e080b0bb2198db936ca9dfd53f5439ba41d82ff5
tiago <tiagohbello@gmail.com>
2023-01-27 15:11:47 -0300

    finalização do fluxo na api de Entrada, Estoque e Saida

--- ivh_inventario/entrada/api/serializers.py
+++ ivh_inventario/entrada/api/serializers.py
@@ -1,11 +1,31 @@
 from rest_framework import serializers
 
 from ivh_inventario.entrada.models import Entrada
+from ivh_inventario.item.models import Item
 
 
 class CRUDEntradaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Entrada
         fields = '__all__'
+
+
+class POSTEntradaSerializer(serializers.Serializer):
+    quantidade = serializers.IntegerField(required=True, allow_null=False)
+    dt_entrada = serializers.DateField(required=False, allow_null=True)
+    usuario = serializers.UUIDField(required=False, allow_null=True)
+    item = serializers.UUIDField(required=False, allow_null=True)
+    is_bem_de_consumo = serializers.BooleanField(required=True, allow_null=False)
+    grupo = serializers.CharField(required=True, max_length=200, allow_null=False)
+    cod = serializers.CharField(max_length=200, required=False, allow_null=True)
+    doc_fiscal = serializers.CharField(max_length=200, required=False, allow_null=True)
+    is_doacao = serializers.BooleanField(required=True, allow_null=False)
+    validade = serializers.DateField(required=False, allow_null=True)
+    val_unit = serializers.DecimalField(max_digits=15, decimal_places=2, required=False, allow_null=True)
+    val_total = serializers.DecimalField(max_digits=15, decimal_places=2, required=False, allow_null=True)
+    fornecedor = serializers.CharField(max_length=200, required=False, allow_null=True)
+    descricao = serializers.CharField(required=True, allow_null=False)
+    tipo_unit = serializers.CharField(max_length=200, required=False, allow_null=True)
+
 
 
--- ivh_inventario/entrada/api/viewsets.py
+++ ivh_inventario/entrada/api/viewsets.py
@@ -1,9 +1,15 @@
-from rest_framework import viewsets
+import datetime
+import uuid
+
+from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from ivh_inventario.entrada.api.serializers import CRUDEntradaSerializer
+from ivh_inventario.entrada.api.serializers import CRUDEntradaSerializer, POSTEntradaSerializer
 from ivh_inventario.entrada.models import Entrada
+from ivh_inventario.estoque.models import Estoque
+from ivh_inventario.item.models import Item
 
 
 class CRUDEntradaViewSet(viewsets.ModelViewSet):
@@ -12,3 +18,43 @@ class CRUDEntradaViewSet(viewsets.ModelViewSet):
     http_method_names = ['get', 'post', 'put', 'patch', 'delete']
     permission_classes = [IsAuthenticated]
     authentication_classes = [TokenAuthentication]
+
+    def create(self, request, *args, **kwargs):
+        self.serializer_class = POSTEntradaSerializer
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        item = Item(
+            is_doacao=request.data.get('is_doacao'),
+            descricao=request.data.get('descricao'),
+            is_bem_de_consumo=request.data.get('is_bem_de_consumo'),
+            grupo=request.data.get('grupo'),
+            cod=request.data.get('cod'),
+            doc_fisc=request.data.get('doc_fisc'),
+            validade=request.data.get('validade'),
+            val_unit=request.data.get('val_unit'),
+            val_total=request.data.get('val_total'),
+            fornecedor=request.data.get('fornecedor'),
+            tipo_unit=request.data.get('tipo_unit')
+        )
+        item.save()
+
+        request.data['item'] = item.pk
+        request.data['dt_entrada'] = datetime.date.today()
+        request.data['usuario'] = self.request.user.pk
+        entrada = Entrada(
+            item=item,
+            dt_entrada=request.data.get('dt_entrada'),
+            usuario=self.request.user,
+            quantidade=request.data.get('quantidade')
+        )
+        entrada.save()
+        estoque = Estoque(
+            estoque_atual=request.data.get('quantidade'),
+            item=item
+        )
+        estoque.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
