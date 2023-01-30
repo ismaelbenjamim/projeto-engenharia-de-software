bbc0b619b7348aae54c031f153e017263c45ad36
ismaelbenjamim <ismaelbenjamim15@hotmail.com>
2023-01-30 11:12:56 -0300

    Implementação de APIs de Redefinição de senha e Esqueci a senha com envio de email

--- ivh_inventario/usuario/models.py
+++ ivh_inventario/usuario/models.py
@@ -3,6 +3,8 @@ from uuid import uuid4
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from ivh_inventario.core.utils.gerador_codigo import geracao_codigo
+
 
 class Usuario(AbstractUser):
     CARGO = (
@@ -13,6 +15,6 @@ class Usuario(AbstractUser):
     )
 
     uuid = models.UUIDField(unique=True, default=uuid4, primary_key=True, editable=False)
-    email = models.EmailField(('email'), unique=False)
+    email = models.EmailField('Email', unique=False)
     cargo = models.CharField(max_length=100, choices=CARGO)
-
+    cod_redefinicao = models.CharField("Código de redefinição de senha", max_length=10, default=geracao_codigo)
