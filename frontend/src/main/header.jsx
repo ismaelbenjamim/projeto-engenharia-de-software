4fa6d5f3e8f2fe17f57fa30289a2f0d530859e6f
ismaelbenjamim <ismaelbenjamim15@hotmail.com>
2023-03-17 13:17:20 -0300

    Removido diretório de frontend

--- frontend/src/main/header.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import { Container, Row, Col, Button, Form, Card } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useNavigate } from 'react-router-dom';
-
-export default function Header() {
-    
-}
