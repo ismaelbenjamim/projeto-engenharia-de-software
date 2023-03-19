8efb23d28cfe6e6f9561849de7ae0ccf716665c4
ismaelbenjamim <ismaelbenjamim15@hotmail.com>
2023-03-18 23:59:31 -0300

    Configuração de deploy

--- ivh_inventario/urls.py
+++ ivh_inventario/urls.py
@@ -50,6 +50,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('', IndexView, name='index'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
                                                                              document_root=settings.MEDIA_ROOT)
