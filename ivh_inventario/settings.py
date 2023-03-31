2e82d3f7d6004dbcba3078daca7f05b7e559566c
ismaelbenjamim <ismaelbenjamim15@hotmail.com>
2023-03-31 17:48:10 -0300

    Configuração no backend e ajustes na API de estoque

--- ivh_inventario/settings.py
+++ ivh_inventario/settings.py
@@ -215,6 +215,9 @@ CORS
 '''
 
 CORS_ORIGIN_ALLOW_ALL = True
-"""CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
-]"""
+# CORS_ORIGIN_WHITELIST = [
+#     'http://localhost:3000'
+# ]
+# CSRF_TRUSTED_ORIGINS = [
+#     'http://localhost:3000'
+# ]
